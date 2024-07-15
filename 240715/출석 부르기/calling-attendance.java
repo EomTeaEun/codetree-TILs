ea1f7c36679bc2be1547d30979efc952af86daeb
EomTeaEun <xodms3320@gmail.com>
2024-07-15 20:50:56 +0900

    Title: 출석 부르기, Time: 114ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240715/출석 부르기/calling-attendance.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if (a == 1) {
+            System.out.println("John");
+        }
+        else if (a ==5) {
+            System.out.println("Vacancy");
+        }
+        
+        else {
+            System.out.println("Vacancy");
+        }
+
+       
+    }
+}
