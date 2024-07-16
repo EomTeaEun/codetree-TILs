74c8db0d910b41f371dbea1f66fd0164cfe9e4f8
EomTeaEun <xodms3320@gmail.com>
2024-07-16 21:45:26 +0900

    Title: b부터 a까지 감소, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240716/b부터 a까지 감소/dec-from-b-to-a.java
+++ 240716/b부터 a까지 감소/dec-from-b-to-a.java
@@ -2,14 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a = sc.nextInt();
 
-        for (int i = n; i >= a ; i --) {
-            System.out.print(i+" ");
-        }
+        // 변수 선언
+        int a, b;
+
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
         
+        // 출력
+        for(int i = b; i >= a; i--)
+            System.out.print(i + " ");
     }
 }
