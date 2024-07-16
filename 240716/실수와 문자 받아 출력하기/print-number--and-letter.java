3da05a6da9a4f6089c615f876ba85e2bdfd2f3b3
EomTeaEun <xodms3320@gmail.com>
2024-07-16 21:20:01 +0900

    Title: 실수와 문자 받아 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240716/실수와 문자 받아 출력하기/print-number--and-letter.java
+++ 240716/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String s = sc.next(); // 문자열 입력
         double c = sc.nextDouble;
-        double d = sc.nextDouble // 입력받은 문자열의 첫 번째 문자 추출
+        double d = sc.nextDouble; // 입력받은 문자열의 첫 번째 문자 추출
         System.out.println(s);
         System.out.printf("%0.2f",c);
         System.out.println();
