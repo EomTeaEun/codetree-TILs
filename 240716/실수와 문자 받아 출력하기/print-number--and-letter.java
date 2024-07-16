5e06c5762fde0721fe904f6c2906ebf7d68237e2
EomTeaEun <xodms3320@gmail.com>
2024-07-16 21:20:53 +0900

    Title: 실수와 문자 받아 출력하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240716/실수와 문자 받아 출력하기/print-number--and-letter.java
+++ 240716/실수와 문자 받아 출력하기/print-number--and-letter.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next(); // 문자열 입력
-        double c = sc.nextDouble;
-        double d = sc.nextDouble; // 입력받은 문자열의 첫 번째 문자 추출
+        double c = sc.nextDouble();
+        double d = sc.nextDouble(); // 입력받은 문자열의 첫 번째 문자 추출
         System.out.println(s);
         System.out.printf("%0.2f",c);
         System.out.println();
