98d03334d9da2c3c10329c72389264d521201b86
EomTeaEun <xodms3320@gmail.com>
2024-07-17 17:34:22 +0900

    Title: 친근하지 않은 수, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240717/친근하지 않은 수/unfriendly-number.java
+++ 240717/친근하지 않은 수/unfriendly-number.java
@@ -11,11 +11,13 @@ public class Main {
         
         for(int i = 1; i <= n; i++) {
             if(i % 2 == 0 || i % 3 == 0 || i % 5 == 0)
-                continue;
-            cnt++;
+            {    
+            
+            System.out.print(i+" ");
+            
+            continue;}
         }
 
-        // 출력
-        System.out.print(cnt);
+        
     }
 }
