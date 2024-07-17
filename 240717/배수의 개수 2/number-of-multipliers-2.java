a2ebb0f2fde0c4d7c6db56a288904caa6d71bd5d
EomTeaEun <xodms3320@gmail.com>
2024-07-17 16:41:39 +0900

    Title: 배수의 개수 2, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240717/배수의 개수 2/number-of-multipliers-2.java
+++ 240717/배수의 개수 2/number-of-multipliers-2.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        
         int cnt = 0;
 
         for(int i = 1 ; i<= 10; i++){
 
-            
+            int n = sc.nextInt();
                 if(n%2 != 0){
                     cnt++;
                     
