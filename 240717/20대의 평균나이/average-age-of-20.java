0e09cd5b5f93a157fca2a78b261711df52ddaa52
EomTeaEun <xodms3320@gmail.com>
2024-07-17 18:02:59 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240717/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = 0;
+        int b = 0;
+        
+        while(true) {
+            // 변수 선언 및 입력
+            
+            int w = sc.nextInt();
+
+            a += w ;
+            cnt ++ ;
+
+            break;
+        }
+
+        a = (double)b/ cnt;
+    }
+}
