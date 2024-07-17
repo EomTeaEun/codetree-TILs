8038e8856359416d039e95ce7272486983e5cf30
EomTeaEun <xodms3320@gmail.com>
2024-07-17 17:22:39 +0900

    Title: 주어진 수까지의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240717/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+       
+       int a = sc.nextInt();
+       int p = 0;
+
+        // 입력을 받으며 0이상 200이하의 정수들을 선별합니다.
+        for(int i = a; i <= b; i++) {
+            p *= i
+        }
+
+        
+        System.out.printf(p);
+    }
+}
