2a9a52b84a2fbd9c89b090c1f039f848b9d3ae42
EomTeaEun <xodms3320@gmail.com>
2024-07-17 17:45:13 +0900

    Title: 1부터의 합, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240717/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        int n;
+        int sum = 0;
+
+        n = sc.nextInt();
+        
+        
+        for(int i = 1; i <= 100; i++) {
+            sum += i;
+            if(sum >= n) {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
