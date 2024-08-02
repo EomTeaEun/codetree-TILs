189afc75ccceec0b48e15c0ee958184cb28e1aef
EomTeaEun <xodms3320@gmail.com>
2024-08-02 15:24:11 +0900

    Title: 사각형의 총 넓이 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240802/사각형의 총 넓이 2/total-width-of-a-rectangle2.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_N = 10;
+    public static final int MAX_R = 200;
+    public static final int OFFSET = 100;
+
+    public static int n;
+    public static int[] x1 = new int[MAX_N];
+    public static int[] y1 = new int[MAX_N];
+    public static int[] x2 = new int[MAX_N];
+    public static int[] y2 = new int[MAX_N];
+
+    public static int[][] checked = new int[MAX_R + 1][MAX_R + 1];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt();
+            y1[i] = sc.nextInt();
+            x2[i] = sc.nextInt();
+            y2[i] = sc.nextInt();
+
+            // OFFSET을 더해줍니다.
+            x1[i] += OFFSET;
+            y1[i] += OFFSET;
+            x2[i] += OFFSET;
+            y2[i] += OFFSET;
+        }
+
+        // 직사각형을 칠해줍니다.
+        // 격자 단위로 진행하는 문제이므로
+        // x2, y2에 등호가 들어가지 않음에 유의합니다.
+        for(int i = 0; i < n; i++)
+            for(int x = x1[i]; x < x2[i]; x++)
+                for(int y = y1[i]; y < y2[i]; y++)
+                    checked[x][y]++;
+        
+        // 직사각형 넓이의 총 합을 구합니다.
+        int area = 0;
+        for(int x = 0; x <= MAX_R; x++)
+            for(int y = 0; y <= MAX_R; y++)
+                if(checked[x][y] > 0)
+                    area++;
+        
+        // 출력
+        System.out.print(area);
+    }
+}
+
+/* import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] arr = new int[201][201];
+        int offset = 100;
+        int cnt = 0;
+
+        for (int k = 0; k < n; k++) {
+            int x1 = sc.nextInt();
+             int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            for (int i = x1; i < x2; i++) { //사각형 채우기
+                for (int j = y1; j < y2; j++) {
+                    arr[i + offset][j + offset] = 1;
+                }
+            }
+
+            for (int l = 0; l < 201; l++) {
+            for (int o = 0; o < 201; o++) {
+                if(arr[l][o]>0){
+                cnt++;
+                }
+            }
+        }
+
+
+
+        }
+
+        
+
+        
+
+        System.out.println(cnt);
+    }
+} */
