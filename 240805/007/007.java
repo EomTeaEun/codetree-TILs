8cb70918cac1fb9b3f9242ede6f53aa1ccddc577
EomTeaEun <xodms3320@gmail.com>
2024-08-05 13:06:50 +0900

    Title: 007, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240805/007/007.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Spy {
+    String sccode;
+    char mtcode;
+    int time;
+
+    public Spy ( String sccode, char mtcode, int time){
+        this.sccode = sccode;
+        this.mtcode = mtcode;
+        this.time = time;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String sCode;
+        char mPoint;
+        int time;
+
+        sCode = sc.next();
+        mPoint = sc.next().charAt(0);
+        time = sc.nextInt();
+        
+        // Spy 객체를 만들어 줍니다.
+        Spy sp = new Spy(sCode, mPoint, time);
+
+        // 결과를 출력합니다.
+        System.out.println("secret code : " + sp.secretCode);
+        System.out.println("meeting point : " + sp.meetingPoint);
+        System.out.println("time : " + sp.time);
+
+
+
+    }
+}
